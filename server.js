bc599d1adeb0d92695181ecb5d77e2750334769c
Daniel L. Weber <dw@thedanielweber.com>
2016-06-14 16:23:33 -0400

    Changed folder structure

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
+var path = require('path');
 var express = require('express');
 var app = express();
 
 // set the port of our application
 // process.env.PORT lets the port be set by Heroku
 var port = process.env.PORT || 8080;
+
+app.set('view engine', 'html');
 
 // make express look in the public directory for assets (css/js/img)
 app.use(express.static(__dirname + '/client'));
