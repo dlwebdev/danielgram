487ae3f76154f1430bfc6672db6bbb3a6b41b6e2
Daniel L. Weber <dw@thedanielweber.com>
2016-06-14 16:28:30 -0400

    Changed folder structure

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ var port = process.env.PORT || 8080;
 
 app.set('view engine', 'html');
 
+app.engine('html', require('ejs').renderFile);
 app.use(express.static(path.join(__dirname, 'index.html')));
 
 // set the home page route
